cb131869437d5eb950d0faac29c447bb963ae369
SB1809 <sophia.babayev@gmail.com>
2025-05-16 15:56:35 +0000

    C

--- workspace/src/gamelogic/level/Level.java
+++ workspace/src/gamelogic/level/Level.java
@@ -193,11 +193,53 @@ public class Level {
 	}
 	
 	
-	//#############################################################################################################
-	//Your code goes here! 
-	//Please make sure you read the rubric/directions carefully and implement the solution recursively!
+	//Precondition: the 
+	//Postcondition:
 	private void water(int col, int row, Map map, int fullness) {
-		
+
+		Water w = new Water (col, row, tileSize, tileset.getImage("Full_water"), this, fullness);
+		if(fullness == 0)
+			w= new Water (col, row, tileSize, tileset.getImage("Falling_water"), this, fullness);
+		else if(fullness == 2)
+			w= new Water (col, row, tileSize, tileset.getImage("Half_water"), this, fullness);
+		else if(fullness == 1)
+			w = new Water (col, row, tileSize, tileset.getImage("Quarter_water"), this, fullness);
+
+		map.addTile(col, row, w);
+
+        //down
+		if(row+1 < map.getTiles()[0].length && !(map.getTiles()[col][row+1] instanceof Water) && !map.getTiles()[col][row+1].isSolid()) {
+			//water(col, row, map, 3);
+			if( row+2 < map.getTiles()[0].length&& map.getTiles()[col][row+2].isSolid()){
+				water(col, row+1, map, 3);
+			}
+			else{
+				water(col, row+1, map, 0);
+			}
+		}
+                       //if we can't go down go left and right.
+		//right
+		else if((row+1 < map.getTiles()[0].length) && !(map.getTiles()[col][row+1] instanceof Water)){
+			if(col+1 < map.getTiles().length && !(map.getTiles()[col+1][row] instanceof Water) && !map.getTiles()[col+1][row].isSolid()) {
+				//water(col+1, row, map, 3);
+				if(fullness >1){
+					water(col+1, row, map, fullness-1);
+				}else{
+					water(col+1, row, map, 1);
+				}
+				
+			}
+			//left
+			if(col-1 >= 0 && !(map.getTiles()[col-1][row] instanceof Water) && !map.getTiles()[col-1][row].isSolid() ) {
+				
+				if(fullness >1){
+					water(col-1, row, map, fullness-1);
+				}else{
+					water(col-1, row, map, 1);
+				}
+
+			}
+		}
 	}
 
 
